c6be57c9f3dc9056d06653bc446312386474e85a
liujoker181 <18721853290@qq.com>
2021-08-30 11:47:16 +0800

    2021-08-30 helloApi 基本的调用返回

--- myapi/src/main/java/club/liujoker/myapi/controller/helloApi.java
+++ myapi/src/main/java/club/liujoker/myapi/controller/helloApi.java
@@ -1,5 +1,18 @@
 package club.liujoker.myapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class helloApi {
+
+    /**
+     * @date 2021-08-30
+     * @description 最基本的返回hello的接口
+     */
+    @RequestMapping("hello")
+    public String helloapi(){
+        return "hello";
+    }
+
 }
